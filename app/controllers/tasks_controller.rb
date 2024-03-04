054b54f2f37ae23d6b8f81182cf0768ac2c56132
nk19963 <kn_1996_0303@yahoo.co.jp>
2024-03-04 22:57:35 +0900

    タスク完了機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,12 @@ class TasksController < ApplicationController
     head :ok
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    head :ok
+  end
+
   private
 
   def task_params
